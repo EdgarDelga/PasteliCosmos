3976e506ca9fa31bffe9ac37d6adae9c97c3c6af
Edgar <95370665+EdgarDelga@users.noreply.github.com>
2022-03-05 19:26:07 -0600

    Delete js directory

--- js/programacion.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function()
-{
-    $("input[name=key]").click(function () {    
-        alert("La edad seleccionada es: " + $('input:radio[name=key]:checked').val());
-        alert("La edad seleccionada es: " + $(this).val());
-    });
-});
